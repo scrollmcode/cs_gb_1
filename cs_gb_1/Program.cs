6d91e0150fc99270610e06394190e00b00687506
ScrollmCode <Supermcode@mail.ru>
2021-02-14 13:50:16 +0300

    Добавьте файлы проекта.

---
+++ cs_gb_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cs_gb_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; // имя
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}, сегодня {DateTime.Now.Date.ToString("dd.MM.yyyy")}");
+        }
+    }
+}
