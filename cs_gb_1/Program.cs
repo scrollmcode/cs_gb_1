a0ec55f58910b6a01bc5cc0c0a0b743252de7492
ScrollmCode <Supermcode@mail.ru>
2021-03-02 20:15:20 +0300

    DZ 5

--- cs_gb_1/Program.cs
+++ cs_gb_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace cs_gb_1
 {
@@ -7,7 +8,45 @@ namespace cs_gb_1
         static void Main(string[] args)
         {
 
-            Console.WriteLine($"Привет.");
+            Console.WriteLine("Задание 1.");
+
+            Console.WriteLine("Введите строку:");
+            string text = Console.ReadLine();
+            string filename = "Задание 1.txt";
+            File.WriteAllText(filename, text);
+
+            Console.WriteLine("\nЗадание 2.");
+
+            string fileStartUpName = "startup.txt";
+            string dateContent = DateTime.Now.ToString() + Environment.NewLine;
+
+            if (File.Exists(fileStartUpName))
+            {
+                File.AppendAllText(fileStartUpName, dateContent);
+            }
+            else
+            {
+                File.WriteAllText(fileStartUpName, dateContent);
+            }
+
+            Console.WriteLine($"Выполнена запись в {fileStartUpName}");
+
+            Console.WriteLine("\nЗадание 3.");
+
+            byte[] massByte = new byte[10];
+
+            Console.WriteLine("Введите 10 чисел через пробел");
+
+            for (int i = 0; i < 10; i++)
+            {
+                massByte[i] = byte.Parse(Console.ReadLine());
+            }
+
+            string byteMassFileName = "byteMassFile.bin";
+            File.WriteAllBytes(byteMassFileName, massByte);
+
+            Console.WriteLine($"Выполнена запись введёных чисел в {byteMassFileName}");
+
         }
     }
 }
