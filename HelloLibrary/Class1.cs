72b3aaecc68d0c88f9ec1aca6e32778dadbdf35b
ScrollmCode <Supermcode@mail.ru>
2021-03-16 19:54:36 +0300

    DZ 8

---
+++ HelloLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloLibrary
+{
+    public class HelloClass
+    {
+        public void WriteHello()
+        {
+            WriteHello("Hello");
+        }
+
+        public void WriteHello(string helloString)
+        {
+            Console.WriteLine(helloString);
+        }
+
+        public string GetHelloString()
+        {
+            return "Hello";
+        }
+    }
+}
